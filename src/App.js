eed757164c527207a309a00c437da95100832583
Jay Patel <jay.p1@ahduni.edu.in>
2020-07-19 00:42:08 +0530

    Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, initialState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button} from 'reactstrap';
 import './App.css';
 
 function App() {
+  //declared a state and function to change a state for this component
+  const [todos, setTodos] = useState(['Take a dog for a walk', 'Take the rubbish out', 'I want to play pubg today']);
+  const [input, setInput ] = useState('')
+
+  console.log('Input is : ', input);
+
+  const addTodo = (event) =>{
+    //this will fire off when we click our todo button
+    setTodos([...todos, ])
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello Guys!</h1>
+      <input type="text" style={{ height: "40px" }} className = 'mb-5' value = {input} onChange = {event => setInput(event.target.value)}/>
+      <Button color="primary" onClick={addTodo}>Add Todo</Button>
+
+      <ul>
+          {todos.map(todo => {
+            return <li>{todo}</li>
+          })}
+      </ul>
+
     </div>
   );
 }
